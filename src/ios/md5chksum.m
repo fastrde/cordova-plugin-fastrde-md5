c58895261e205a75e1130448206771af4e95fca1
Fabian Strachanski <strachanski@googlemail.com>
2016-09-13 12:49:07 +0200

    removed uppercase

--- src/ios/md5chksum.m
+++ src/ios/md5chksum.m
@@ -41,7 +41,7 @@
 		CC_MD5_Final(digest, &md5);
 		NSString* hex = [NSString stringWithFormat: @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",digest[0], digest[1], digest[2], digest[3], digest[4], digest[5], digest[6], digest[7], digest[8], digest[9], digest[10], digest[11], digest[12], digest[13], digest[14], digest[15]];
 		if (hex != nil && [hex length] > 0) {
-			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[hex uppercaseString]];
+			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:hex];
 		} else {
 			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
 		}
